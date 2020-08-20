cd9f710e3d2783329d84adbaf99e876bce2d8a60
meyash <yashsn2127@gmail.com>
2020-08-20 14:41:58 +0530

    -exer

--- query_lib/Scraper.py
+++ query_lib/Scraper.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-# from selenium import webdriver
+from selenium import webdriver
 import os
 options = Options()
 
